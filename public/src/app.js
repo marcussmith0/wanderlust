28b7cd8665e4eeed2132f8c5ce0bbb587e5dd256
Darrell Freeman <nclespaul@yahoo.com>
2017-08-18 18:08:06 -0500

    Added files for Geolocation module

---
+++ public/src/app.js
@@ -0,0 +1,14 @@
+// Include the Main React Dependencies
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// Include the Main Component
+var Main = require("./components/Main");
+
+// This code here allows us to render our main component (in this case "Main")
+ReactDOM.render(
+    <div className="main-container">
+        <Main />
+    </div>
+    , document.getElementById("app")
+);
