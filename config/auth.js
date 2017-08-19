ee3fe1bad9e46c7a16c66f3ed615a7c692fe7de7
Marcus Smith <marcussmith0711@gmail.com>
2017-08-19 11:51:21 -0500

    added the config folder  and auth file

---
+++ config/auth.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    'googleAuth' : {
+
+        'clientID'      : '295129897174-cla0v7tp130ci25ivcj609rek8qdmcgv.apps.googleusercontent.com',
+        'clientSecret'  : 'RjkSeDumqFCL4Sm4u3gbpAaT',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        
+    }
+
+};
