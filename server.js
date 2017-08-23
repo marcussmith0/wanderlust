9ff0bdb555db6e0ac5b79da166eb2e6bb36d0b6d
Erica Rodriguez <erica.rodriguez.mail@gmail.com>
2017-08-22 19:43:46 -0500

    committing changes

--- server.js
+++ server.js
@@ -9,13 +9,6 @@ var bodyParser   = require('body-parser');
 var session      = require('express-session');
 var flash        = require('connect-flash');
 var passport     = require('passport');
-<<<<<<< HEAD
-// TOOL TO MAKE AJAX REQUESTS
-var fetch = require('node-fetch');
-var querystring = require('querystring');
-
-=======
->>>>>>> cfc2e24c9c3e9114b203d71478ac8d322dc8a0b4
 
 var app = express();
 const PORT = process.env.PORT || 8080;
@@ -59,62 +52,6 @@ require('./routes/yelp-routes')(app);
 require('./routes/routes')(app);
 require("./routes/auth-routes")(app, passport);
 
-<<<<<<< HEAD
-var port = app.get('port');
-
-// TOOL TO MAKE URL STRINGS: https://nodejs.org/api/querystring.html
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-app.get('/', function(req, res) {
-    res.send("It works!");
-});
-
-app.get('/api/:postalCode', function (req, res) {
-    // ENTER YOUR SEARCH TERM HERE
-    var searchTerm = "";
-    var zipCode = req.params.postalCode;
-    var qs = querystring.stringify({
-        term: searchTerm,
-        location: zipCode
-    });
-    // ENTER YOUR API URL
-    var url = "https://api.yelp.com/v3/businesses/search?" + qs;
-    var params = {
-        headers: {
-            Authorization: "Bearer W6DsQNPFIyD6bXdUTGnRj3F47JJQq60H1xXAX82YHQQ1GM-yedZcm0SR-VMFn726zLkvAR32d_DTp4WpCXxVvf5mrWCaUWrb51Z7A9UhbvjlqYrHrEKK9ZB6Ik_5WHYx"
-        },
-        method: "GET",
-        // body: {
-            // location: zipCode,
-            // term: searchTerm
-        // }
-    };
-    console.log("Zip code is " + zipCode);
-    
-    /**
-    * For our request to the Yelp API, we'll use the Fetch API
-    * 
-    * Reading: https://davidwalsh.name/fetch
-    * Documentation: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-    */
-    fetch(url, params)
-    .then(resYelp => resYelp.json()) // Return JSON and pass it to the next `then`
-    .then(yelpJson => res.send(yelpJson))
-    .catch(function (err) {
-        console.log("An error occurred");
-        res.send(err);
-    });
-});
-
-app.listen(port, function () {
-    console.log('App running at ' + port);
-=======
 app.listen(PORT, function () {
     console.log('App running at ' + PORT);
->>>>>>> cfc2e24c9c3e9114b203d71478ac8d322dc8a0b4
 });
